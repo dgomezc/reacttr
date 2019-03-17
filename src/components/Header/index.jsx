c3a99c3bd8cc98b5bb9102bac27c31c62e4a0d9a
David Gómez <v-davgom@microsoft.com>
2019-03-17 20:37:22 +0100

    Add basic components and remove any files

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import styles from "./Header.module.css"
+
+class Header extends Component {
+    render ()
+    {
+        return (
+            <header className={styles.root}>
+                <h1 className={styles.logo}>Reactrr</h1>
+            </header>
+        )
+    }
+}
+
+export default Header
